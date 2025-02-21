7f7622e682393c84ee1626c1e1edad8daa969ae1
clifforddonk <clifforddonk@gmail.com>
2025-02-21 17:49:03 +0000

    first commit

--- src/com/Clifford/NumberGuess.java
+++ src/com/Clifford/NumberGuess.java
@@ -1,7 +1,20 @@
 package com.Clifford;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class NumberGuess {
     public static void generator() {
+        Scanner scanner= new Scanner(System.in);
+        Random rand= new Random();
+
+       int randomNum=rand.nextInt(10);
+
+        System.out.println("Guess any number from 1 to 10:");
+        int num=scanner.nextInt();
+
+        if (num==randomNum)
+            System.out.println("Congratulation, You won");
 
 
     }
