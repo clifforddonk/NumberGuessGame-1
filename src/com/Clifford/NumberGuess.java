ead66404e6f1bce68eec0ea324d95a254aead3ef
clifforddonk <clifforddonk@gmail.com>
2025-02-23 17:13:37 +0000

    Add control flow

--- src/com/Clifford/NumberGuess.java
+++ src/com/Clifford/NumberGuess.java
@@ -5,25 +5,35 @@ import java.util.Scanner;
 
 public class NumberGuess {
     public static void generator() {
-        Scanner scanner= new Scanner(System.in);
-        Random rand= new Random();
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
 
-       int randomNum=rand.nextInt(10);
-       int num=0;
+        // Fix 1: Ensure random number is between 1 and 10
+        int randomNum = rand.nextInt(10) + 1;
+        int num = 0;
 
 
-       do{
-        System.out.print("Guess any number from 1 to 10:");
-        num=scanner.nextInt();
-        System.out.println("Invalid input!Enter a number from 1 to 10");
-        }while(num>10||num<0);
+        do {
+            System.out.print("Guess any number from 1 to 10: ");
+            num = scanner.nextInt();
 
-        if (num==randomNum)
-            System.out.println("Congratulation, You won");
-        else if (num>randomNum)
-            System.out.println("You are wrong!😢😢\n" +"The number is lower than "+ num);
+
+            if (num > 10 || num < 1) {
+                System.out.println("Invalid input! Enter a number from 1 to 10.");
+            }
+
+        } while (num > 10 || num < 1);
+
+
+        if (num == randomNum)
+            System.out.println("Congratulations! 🎉 You won!");
+        else if (num > randomNum)
+            System.out.println("You are wrong! 😢 The number is lower than " + num);
         else
-            System.out.println("You are wrong!😢😢\n"+ "The number is greater than "+ num);
-        System.out.println("The correct  number was "+randomNum);
+            System.out.println("You are wrong! 😢 The number is greater than " + num);
+
+        System.out.println("The correct number was " + randomNum);
+        scanner.close();
     }
+
 }
